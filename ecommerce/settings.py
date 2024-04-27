1ac586f80be383d36357335acce00cde8432a58d
ujjwal <srivastavaaman2017@gmail.com>
2024-04-27 08:40:27 +0530

    taken pull from newapi

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ujjwal1102.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*","ujjwal1102.pythonanywhere.com"]
 
 
 # Application definition
